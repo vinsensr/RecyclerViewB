0dbffb1840c99e07d53ef4d48db5b92366df6928
vinsensr <92628962+vinsensr@users.noreply.github.com>
2022-05-27 14:47:17 +0700

    Initial commit

---
+++ app/src/main/java/com/vinsensr/recyclerviewb/MainActivity.java
@@ -0,0 +1,50 @@
+package com.vinsensr.recyclerviewb;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import java.util.List;
+
+import cz.msebera.android.httpclient.Header;
+
+public class MainActivity extends AppCompatActivity
+{
+    private RecyclerView _recyclerView1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        _recyclerView1 = (RecyclerView) findViewById(R.id.recyclerView1);
+
+        AsyncHttpClient ahc = new AsyncHttpClient();
+        String url = "https://stmikpontianak.net/011100862/tampilMahasiswa.php";
+
+        ahc.get(url, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                Gson g = new Gson();
+                List<MahasiswaModel> mahasiswaModelList = g.fromJson(new String(responseBody), new TypeToken<List<MahasiswaModel>>(){}.getType());
+
+                RecyclerView.LayoutManager lm = new LinearLayoutManager(MainActivity.this);
+
+                MahasiswaAdapter ma = new MahasiswaAdapter(mahasiswaModelList);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
